c3130b7ddf7a247b40f2b898e005add014824697
CoderTrader <jrubyist@gmail.com>
2013-09-14 09:07:21 -0500

    fix for gravatars not showing up due to rating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def avatar_url(contributor)
     gravatar_id = Digest::MD5.hexdigest(contributor.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&rating=x"
   end
 
   def flash_class(level)
