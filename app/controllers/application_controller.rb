31ec6e7d88fcbcecaf4cd5fe165aa70a60fdc208
CoderTrader <jrubyist@gmail.com>
2013-09-23 09:26:37 -0500

    disable turbolinks to allow jquery to work correctly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :clear_cache
+
+  def clear_cache
+    headers['Last-Modified'] = Time.now.httpdate 
+  end
+ 
+
   private
 
   def not_authenticated
